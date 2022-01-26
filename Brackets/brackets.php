692d47037a7467326ee7cfd44c7143e17d012526
old-amigo <dmitry.gwork@gmail.com>
2022-01-26 19:10:54 +0300

    Done brackets task

--- Brackets/brackets.php
+++ Brackets/brackets.php
@@ -1,1 +1,18 @@
 <?php
+    $statement = readline();
+
+    $openBrackets = 0;
+    $closeBrackets = 0;
+    $error = false;
+
+    for ($i = 0; $i < strlen($statement); $i++){
+        if ($statement[$i] == '(') $openBrackets++;
+        if ($statement[$i] == ')') $closeBrackets++;
+        if ($closeBrackets > $openBrackets){
+            $error = true;
+            break;
+        }
+    }
+
+    if ($closeBrackets != $openBrackets) $error = true;
+    ($error)? print('Statement is wrong.') : print('Statement is correct');
