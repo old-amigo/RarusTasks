17e1c87cc620c51144924c1185761b223317b6c7
old-amigo <dmitry.gwork@gmail.com>
2022-01-26 19:28:08 +0300

    Adding comments, miserable corrections

--- Brackets/brackets.php
+++ Brackets/brackets.php
@@ -1,4 +1,12 @@
 <?php
+    /*
+     * Программа для проверки на корректность расстановки скобок в выражении
+     *
+     * Скобки расставлены верно, если:
+     * 1) Количество открывающих и закрывающих скобок совпадает
+     * 2) Нет такой закрывающей скобки, которой бы не предшествовала открывающая
+     */
+
     $statement = readline();
 
     $openBrackets = 0;
@@ -8,11 +16,14 @@
     for ($i = 0; $i < strlen($statement); $i++){
         if ($statement[$i] == '(') $openBrackets++;
         if ($statement[$i] == ')') $closeBrackets++;
-        if ($closeBrackets > $openBrackets){
+
+        if ($closeBrackets > $openBrackets){    //проверка условия 2)
             $error = true;
             break;
         }
     }
 
+    // проверка условия 1)
     if ($closeBrackets != $openBrackets) $error = true;
-    ($error)? print('Statement is wrong.') : print('Statement is correct');
+
+    ($error)? print('Statement is wrong.') : print('Statement is correct.');
